87133956ff5987b2e82858bda2cf653bed3f6819
2toxic <isaqtm@gmail.com>
2018-09-29 22:39:57 +0300

    Provide cors destinations

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ host = 'localhost'
 port = 8000
 debug = True
 
-CORS_DESTINATION = r'[.]*'
+CORS_DESTINATION = r'(localhost|pivo.0sk.in)'
 SQLALCHEMY_DATABASE_URI = 'postgres://pivo:shaurma1337@pivo.0sk.in/pivodb'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
