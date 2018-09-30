acd544daf79e015d863ac293a17e4960bc36cc7c
whimo <me@whimo.me>
2018-09-30 08:27:15 +0300

    Model save and load methods, also some fixes

--- app/recommender.py
+++ app/recommender.py
@@ -1,12 +1,15 @@
 from app import app, models
 from lightfm import LightFM
 from lightfm.data import Dataset
+import pickle
 import numpy as np
 
 
 LIGHTFM_LOSS = 'warp'
 LIGHTFM_COMPONENTS = 32
 LIGHTFM_EPOCHS = 8
+
+FILENAME = 'recommender.pkl'
 
 
 class PositiveRecommender:
@@ -22,7 +25,7 @@ class PositiveRecommender:
                          place_ids if place_ids else (place.id for place in models.Place.query.distinct()))
 
         self.user_model_map = self.dataset.mapping()[0]
-        self.model_place_map = {v: k for k, v in self.dataset.mapping()[2]}
+        self.model_place_map = {v: k for k, v in self.dataset.mapping()[2].items()}
 
         self.n_places = self.dataset.interactions_shape()[1]
 
@@ -31,17 +34,28 @@ class PositiveRecommender:
 
         self.epochs = epochs
 
-    def fit(self, reviews=None):
+    def fit(self, reviews=None, save=True):
         if not reviews:
-            reviews = models.Review.query
+            reviews = models.Review.query.filter(models.Review.rating > 4)
 
         interactions, weights = self.dataset.build_interactions(((review.user_id, review.place_id)
                                                                  for review in reviews))
         self.model.fit(interactions, epochs=self.epochs)
 
+        if save:
+            self.save()
+
     def recommend(self, user_id):
-        model_user_id = self.dataset.mapping()[2][user_id]
+        model_user_id = self.user_model_map[user_id]
         scores = self.model.predict(model_user_id, np.arange(self.n_places))
         places_ranking = self.place_ids[np.argsort(-scores)]
 
         return places_ranking
+
+    def save(self, filename=FILENAME):
+        with open(filename, 'wb') as f:
+            pickle.dump(self.__dict__, f, 2)
+
+    def load(self, filename=FILENAME):
+        with open(filename, 'rb') as f:
+            self.__dict__.update(pickle.load(f))
